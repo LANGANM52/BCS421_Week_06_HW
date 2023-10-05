321d27984a2e742209d43875cbffdb8625e9778c
LANGANM52 <michaellangan25@gmail.com>
2023-10-05 15:20:04 -0400

    Added one Seekbar view and make it change the text as shown in the following video.

--- app/src/main/java/edu/farmingdale/bcs421/bcs421_f22_w6_fragmentsdemo/MainActivity2.kt
+++ app/src/main/java/edu/farmingdale/bcs421/bcs421_f22_w6_fragmentsdemo/MainActivity2.kt
@@ -3,20 +3,26 @@ package edu.farmingdale.bcs421.bcs421_f22_w6_fragmentsdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.SeekBar
 
 class MainActivity2 : AppCompatActivity() {
     lateinit var mBtn1:Button
     lateinit var mBtn2:Button
     lateinit var mTV:TextView
+    lateinit var mSeekBar: SeekBar
+    lateinit var mEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        mBtn1 = findViewById(R.id.btn1);
-        mBtn2 = findViewById(R.id.btn2);
+        mBtn1 = findViewById(R.id.btn1)
+        mBtn2 = findViewById(R.id.btn2)
         mTV = findViewById(R.id.tv1)
+        mSeekBar = findViewById(R.id.seekBar)
+        mEditText = findViewById(R.id.editText01)
         var frgmnt01 = Fragment01()
         var frgmnt02 = Fragment02()
 
@@ -34,10 +40,23 @@ class MainActivity2 : AppCompatActivity() {
             }
             readFromSharedPref()
         }
+        mSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val textSize = (15 + progress).toFloat()
+                mTV.textSize = textSize
+                findViewById<EditText>(R.id.editText01).setText(textSize.toString())
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
     }
 
     private fun readFromSharedPref(){
         var sharedPref= getSharedPreferences(MainActivity().SHAREDPREF_FILENAME, MODE_PRIVATE)
-        mTV.setText(sharedPref.getString("KEY", "not forund"))
+        mTV.setText(sharedPref.getString("KEY", "not found"))
     }
 }
